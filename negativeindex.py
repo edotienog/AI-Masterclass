7aae22c324ab64531012aea6b9ec03b012b25af2
edotienog <otieno1edwin@outlook.com>
2026-08-24 16:22:34 +0300

    Indexing and len of list

--- negativeindex.py
+++ negativeindex.py
@@ -1,6 +1,11 @@
-# Negative indexing 
+# Negative indexing and Length of a list
 
 weekly_steps = [9200, 10500, 8800, 11000, 7600, 9400, 10200]
 print("Last day:", weekly_steps [-1]) # the last index is usually -1
 print("Second last day:"), weekly_steps[-2]
 print("First day:", weekly_steps [-7])
+print("Days tracked:", len(weekly_steps))
+
+skills = ["welding", "tiling", "upholstery", "phone repair", "copywriting"]
+print("Skills in List:", len(skills))
+print("Index 4 on the list:", skills [3])
