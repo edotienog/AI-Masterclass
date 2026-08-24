a04c51cd9f6cbddb72e809b397ba5c66babcdacb
edotienog <otieno1edwin@outlook.com>
2026-08-24 15:59:10 +0300

    List and indexing in python

---
+++ lists.py
@@ -0,0 +1,9 @@
+# List and indexing
+
+daily_habit = ["8 glasses water", "8000 steps", "cold shower", "OMAD fast", "workout"]
+print("Position 1:", daily_habit[0])
+print("Position 2:", daily_habit[1])
+print("Position 3:", daily_habit[2])
+print("Position 4:", daily_habit[3])
+print("Last item:", daily_habit[4])
+
