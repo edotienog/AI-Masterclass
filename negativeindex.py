1193b3f3da568931e70cebdfabcb76a901bf8603
edotienog <otieno1edwin@outlook.com>
2026-08-24 16:16:44 +0300

    Updated negative indexing

--- negativeindex.py
+++ negativeindex.py
@@ -2,3 +2,5 @@
 
 weekly_steps = [9200, 10500, 8800, 11000, 7600, 9400, 10200]
 print("Last day:", weekly_steps [-1]) # the last index is usually -1
+print("Second last day:"), weekly_steps[-2]
+print("First day:", weekly_steps [-7])
