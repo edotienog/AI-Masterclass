648824030331b22cf0a00e1ae05971edb1a1ab12
edotienog <otieno1edwin@outlook.com>
2026-08-25 23:46:05 +0300

    using index method to find items in list

---
+++ findingitem.py
@@ -0,0 +1,5 @@
+# using index to locate item in a list 
+
+habits = ["8 glasses water", "cold shower", "OMAD fast", "workout"]
+position = habits.index("OMAD fast")
+print("OMAD fast is at postion:", position)
