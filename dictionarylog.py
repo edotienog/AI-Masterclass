66e17288187910918121b0598d49ff16504ee91b
edotienog <otieno1edwin@outlook.com>
2026-08-26 00:19:56 +0300

    First dictionary and key value example

---
+++ dictionarylog.py
@@ -0,0 +1,10 @@
+# Creating a simple dictionary with key-values using curly {}
+
+daily_log = {
+    "steps": 9200,
+    "water_glasses": 8,
+    "fasting_protocol" :"OMAD",
+    "sleep_hours": 7.5
+}
+
+print(daily_log)
