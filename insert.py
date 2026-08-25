79ae4eab04524965fd284f992aa8b116076dc378
edotienog <otieno1edwin@outlook.com>
2026-08-25 14:24:14 +0300

    insert methods specific index

---
+++ insert.py
@@ -0,0 +1,11 @@
+# insert() method add item to a specific position within the list
+
+#Before
+skills = ["welding", "plumbing", "carpentry", "upholstery"]
+print("Before:", skills)
+print()
+
+#After adding a new skills using insert
+skills.insert(2, "Data science and Machine Learning")
+print("After:", skills)
+
