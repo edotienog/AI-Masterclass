24c591cb634baa710de6b442f891fa3c19c1e609
edotienog <otieno1edwin@outlook.com>
2026-08-25 23:47:20 +0300

    sort method arrange list in ascending order

---
+++ sort.py
@@ -0,0 +1,9 @@
+# Using sort method to rearrange list in ascending order
+
+weekly_steps = [9200, 10500, 8800, 11000, 7600, 9400, 10200]
+print("Unsorted:", weekly_steps)
+print()
+#Sorted list 
+
+weekly_steps.sort()
+print("Sorted low to high:", weekly_steps )
