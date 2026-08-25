3d8c1b8bc7a0c8a46ee8e1c9b8b4e781c38bfcae
edotienog <otieno1edwin@outlook.com>
2026-08-25 23:45:15 +0300

    using count() method

---
+++ count.py
@@ -0,0 +1,9 @@
+# Count () used for counting how many times an item appears on list
+
+daily_habit = ["hit", "miss", "hit", "hit", "miss", "hit", "hit"]
+
+hit_count = daily_habit.count("hit")
+miss_count = daily_habit.count("miss")
+print("Days goal hit:", hit_count)
+print()
+print("Days missed:", miss_count)
---
+++ remove.py
@@ -0,0 +1,7 @@
+#Using remove() method to delete item in a list
+
+habits = ["8 glasses water", "cold shower", "OMAD fast", "workout", "cold shower" ]
+print("Before:", habits)
+print()
+habits.remove("cold shower") #Remove on the first item in the list
+print("After:", habits)
