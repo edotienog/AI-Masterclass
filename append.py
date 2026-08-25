a0ea45d617821199418bf6b30d63b8ffbb72aa96
edotienog <otieno1edwin@outlook.com>
2026-08-25 09:25:39 +0300

    append method add item

---
+++ append.py
@@ -0,0 +1,12 @@
+# Adding items in list using append() metho
+
+#Before
+skills = ["welding", "tiling","upholstery"]
+print("Before:", skills)
+print()
+#After
+skills.append("phone repair")
+print("After:", skills)
+
+skills.append("graphic design")
+print("Final:", skills)
