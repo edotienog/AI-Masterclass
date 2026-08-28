7b1154eb91b0b947637991fe19f98e37a8d88fbf
edotienog <otieno1edwin@outlook.com>
2026-08-28 19:43:57 +0300

    using loop in dict list

--- dictexercise.py
+++ dictexercise.py
@@ -5,10 +5,10 @@ week_log =[
     {"Day": "Tuesday", "steps": 10500, "protocol": "2MAD"},
     {"Day": "Wednesday", "steps": 8800, "protocol": "OMAD"},
     {"Day": "Thursday", "steps": 11000, "protocol": "Autophagy Marathon"},
-    {"Day": "Frida", "steps": 7600, "protocol": "OMAD"},
+    {"Day": "Friday", "steps": 7600, "protocol": "OMAD"},
 ]
 
-total = 0 
+total = 0 # Initial the value to 0 ready to store the sum of steps
 for log in week_log:
     print(log["Day"], "|", log["steps"], "|", log["protocol"])
     total += log["steps"] # adds total steps for the week
