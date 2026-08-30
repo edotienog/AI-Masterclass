fd0136a3446cc6a3160433e92f7849becf1bfbf0
edotienog <otieno1edwin@outlook.com>
2026-08-30 20:55:32 +0300

    defining function in python

---
+++ function.py
@@ -0,0 +1,11 @@
+# A reusable block of code
+
+def show_daily_goal():
+    print("Steps goal: 8,000 steps")
+    print("Water: 8 glasses")
+    print("Cold shower: Yes")
+show_daily_goal()
+print("---")
+show_daily_goal()
+print("---")
+show_daily_goal()
